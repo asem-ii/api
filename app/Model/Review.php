34af33ac553fa499a71060a33d8e8d97ee3fa76f
asem-ii <asemjason@gmail.com>
2019-01-16 21:39:04 +0000

    Relationships Added

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Course;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function course()
+    {
+    	return $this->belongsTo(Course::class);
+    }
 }
