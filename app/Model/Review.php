b01d73738a873f5fc0715f087e4f924c38c5dc84
asem-ii <asemjason@gmail.com>
2019-01-18 11:48:50 +0000

    Reviews Created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+	protected $fillable = [
+		'star','student','review'
+	];
+
+
     public function course()
     {
     	return $this->belongsTo(Course::class);
